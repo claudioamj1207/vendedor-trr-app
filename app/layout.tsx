29882949169bf16cb4a0f382e2b3a38e9f6dd958
claudioamj1207 <claudioamj@gmail.com>
2026-04-08 19:01:49 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,14 @@
+export const metadata = {
+  title: 'Vendedor TRR',
+  description: 'Sistema de Gestão',
+}
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body style={{ margin: 0, background: 'black' }}>
+        {children}
+      </body>
+    </html>
+  )
+}
