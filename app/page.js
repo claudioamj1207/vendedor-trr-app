7bc74f6fbea06cb421848e267decf7f53f687c12
claudioamj1207 <claudioamj@gmail.com>
2026-04-08 21:03:35 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic';
+import './globals.css'; // <-- ESTA É A LINHA QUE TRAZ O VISUAL E AS CORES DE VOLTA
 
 const AppContent = dynamic(() => import('./AppContent'), {
   ssr: false,
